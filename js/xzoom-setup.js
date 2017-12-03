97b9b2d645f1112e98a8e47f2d3e11f7a3f40f5c
Filipe Carneiro <filipe.carneiro@doutorpc.net>
2017-12-03 22:51:47 +0000

    Basic HTML

---
+++ js/xzoom-setup.js
@@ -0,0 +1,5 @@
+﻿(function ($) {
+    $(document).ready(function () {
+        $('.xzoom, .xzoom-gallery').xzoom({ position: 'lens', lensShape: 'circle', sourceClass: 'xzoom-hidden', title: true });
+    });
+})(jQuery);
